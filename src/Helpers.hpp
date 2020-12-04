58efc7f357e2f7fadad1612d8715d54f09b5b15f
Sebastian Dreßler <sebastian@swarm64.com>
2020-12-04 20:17:06 +0100

    Add Helpers.hpp

---
+++ src/Helpers.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "arrow/api.h"
+
+extern "C" {
+#include "postgres.h"
+}
+
+static int
+get_arrow_list_elem_type(arrow::DataType *type)
+{
+    auto children = type->fields();
+
+    Assert(children.size() == 1);
+    return children[0]->type()->id();
+}
